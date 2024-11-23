cb08975e46a4b505a7b84798007102ce123a6854
Harrison Bounds <harrison.bounds777@gmail.com>
2024-11-22 20:31:22 -0600

    typo fix

--- doodle_droid/doodle_droid/process.py
+++ doodle_droid/doodle_droid/process.py
@@ -2,7 +2,7 @@ import rclpy
 from rclpy.node import Node
 from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
-fromlinedraw import linedraw
+from linedraw import linedraw
 
 
 class Image_Processing_Node(Node):
