c8c6227f314bed1795f56948da8a5d28b7f22a0f
Harrison Bounds <harrison.bounds777@gmail.com>
2024-11-22 20:00:39 -0600

    Linedraw submodule and processing outline

---
+++ doodle_droid/doodle_droid/process.py
@@ -0,0 +1,42 @@
+import rclpy
+from rclpy.node import Node
+import linedraw.linedraw as ld
+from cv_bridge import CvBridge
+from sensor_msgs.msg import Image
+
+class Image_Processing_Node(Node):
+    def __init__(self):
+        super().__init__('image_processing_node')
+        self.image_sub = self.create_subscription(Image, '/image_raw', callback=self.image_processing_callback)
+        self.take_picture = self.create_service(Image, 'take_picture', self.get_image)
+        self.pub = self.create_publisher(Image, 'new_image', 10)
+        self.bridge = CvBridge()
+        self.new_image = None
+        
+
+    def image_processing_callback(self, msg):
+        self.old_image = msg
+    
+    def get_image(self):
+        self.new_image = self.old_image
+        self.linedraw()
+        
+    def linedraw(self):
+        cv_image = self.bridge.imgmsg_to_cv2(self.new_image, desired_encoding='bgr8')
+        lines = ld.sketch(cv_image)
+        lined_image = self.bridge.cv2_to_imgmsg(lines, encoding='bgr8')
+        
+        self.pub.publish(lined_image)
+
+def main(args=None):
+    """Entrypoint for the mynode ROS node."""
+    rclpy.init(args=args)
+    node = Image_Processing_Node()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    import sys
+    main(sys.argv)
--- doodle_droid/setup.py
+++ doodle_droid/setup.py
@@ -4,12 +4,13 @@ package_name = 'doodle_droid'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='1.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name, ['package.xml',
+                                   'launch/camera.launch.xml']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -20,6 +21,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'image_processing_node = doodle_droid.process:main'
         ],
     },
 )
