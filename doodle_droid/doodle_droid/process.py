c6e79ad68f699c6e6969d45089a6876bc7003769
Harrison Bounds <harrison.bounds777@gmail.com>
2024-11-24 12:12:02 -0600

    process changes

--- doodle_droid/doodle_droid/process.py
+++ doodle_droid/doodle_droid/process.py
@@ -7,7 +7,7 @@ from sensor_msgs.msg import CompressedImage
 from std_srvs.srv import Empty
 from cv_bridge import CvBridge
 import cv2 as cv
-from doodle_droid.linedraw import linedraw
+from doodle_droid.linedraw.linedraw import *
 
 
 class ImageProcessingNode(Node):
@@ -18,6 +18,7 @@ class ImageProcessingNode(Node):
         self.take_picture_service = self.create_service(Empty, '/take_picture', self.capture_image)
         self.processed_image_pub = self.create_publisher(CompressedImage, '/new_image', 10)
         self.current_image = None
+        self.absolute_path = '/home/harrison-bounds/ws/ES_HW/doodle_droid/src/DoodleDroid/doodle_droid/doodle_droid/images/output.jpg'
         self.bridge = CvBridge()
         
     def get_image_callback(self, msg):
@@ -25,9 +26,9 @@ class ImageProcessingNode(Node):
     
     def capture_image(self, request, response):
         cv_image = self.bridge.compressed_imgmsg_to_cv2(self.current_image, desired_encoding='passthrough')
-        cv.imwrite("images/output.jpg", cv_image) #Convert numpy array tp jpg for image processing
+        cv.imwrite(self.absolute_path, cv_image) #Convert numpy array tp jpg for image processing
         self.get_logger().info("Begin processing")
-        lined_image = linedraw.sketch("images/output.jpg")
+        lined_image = linedraw.sketch(self.absolute_path)
         self.get_logger().info("Finished processing")
         
         return response
