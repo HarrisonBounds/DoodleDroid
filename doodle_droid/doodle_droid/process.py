e06ec773cf54fc910da64ee99ed8d9a40f5af544
Harrison Bounds <harrison.bounds777@gmail.com>
2024-12-09 12:30:27 -0600

    Accept and reject service implemented

--- doodle_droid/doodle_droid/process.py
+++ doodle_droid/doodle_droid/process.py
@@ -3,16 +3,14 @@ import rclpy
 import rclpy.parameter
 from rclpy.node import Node
 import rclpy.time
-
 from sensor_msgs.msg import CompressedImage
-from std_srvs.srv import Empty
+from std_srvs.srv import Empty, SetBool
 from cv_bridge import CvBridge
 import cv2 as cv
 from doodle_droid.linedraw.linedraw import *
 from std_msgs.msg import String
 import json
 from ament_index_python.packages import get_package_share_directory
-import rospkg
 
 
 class ImageProcessingNode(Node):
@@ -21,6 +19,7 @@ class ImageProcessingNode(Node):
 
         self.image_sub = self.create_subscription(CompressedImage, '/image_raw/compressed', self.get_image_callback, 10)
         self.take_picture_service = self.create_service(Empty, '/take_picture', self.capture_image)
+        self.take_picture_service = self.create_service(SetBool, '/accept', self.accept_image)
         self.processed_image_pub = self.create_publisher(String, '/new_image', 10)
         self.current_image = None
         self.pkg_name = "doodle_droid"
@@ -33,6 +32,9 @@ class ImageProcessingNode(Node):
         
         self.get_logger().info(f"SELF.PS: {self.pkg_share}")
         self.get_logger().info(f"SELF.PATH: {self.path}")
+        
+        self.msg = None
+        self.normalized_data = []
         
     def get_image_callback(self, msg):
         self.current_image = msg
@@ -56,14 +58,9 @@ class ImageProcessingNode(Node):
             cv.waitKey(1) 
             
             lined_image = doodle_droid.linedraw.linedraw.sketch(image_array)
-            self.get_logger().info(f"Number of strokes: {len(lined_image)} ")
             
             self.get_logger().info(f"Number of strokes: {len(lined_image)} ")
             self.get_logger().info("Finished processing")
-            
-        
-            
-        self.get_logger().info(f"lined image: {lined_image}")
             
         all_values = [value for sublist in lined_image for tuple_item in sublist for value in tuple_item]
 
@@ -72,7 +69,7 @@ class ImageProcessingNode(Node):
         max_val = max(all_values)
 
         # Step 2: Normalize each tuple
-        normalized_data = []
+        #normalized_data = []
         for sublist in lined_image:
             normalized_sublist = []
             for value in sublist:
@@ -80,18 +77,28 @@ class ImageProcessingNode(Node):
                 normalized_tuple = tuple((component - min_val) / (max_val - min_val) for component in value)
                 normalized_sublist.append(normalized_tuple)
                 
-            normalized_data.append(normalized_sublist)
-
-        self.get_logger().info(f"Normalized list to publish: {normalized_data}")
-        
-        json_data = json.dumps(normalized_data)
-        msg = String()
-        msg.data = json_data
-        self.processed_image_pub.publish(msg)
-        
-        self.get_logger().info(f"DONE PUBLISHING")
+            self.normalized_data.append(normalized_sublist)
               
         return response
+    
+    def accept_image(self, request, response):
+        if request.data:
+            self.get_logger().info(f"Normalized list to publish: {self.normalized_data}")
+        
+            json_data = json.dumps(self.normalized_data)
+            msg = String()
+            msg.data = json_data
+            self.processed_image_pub.publish(msg)
+            
+            self.get_logger().info(f"DONE PUBLISHING")
+            
+            response.success = True
+            
+        else:
+            response.success = False
+            
+        return response
+        
 
 def main(args=None):
     rclpy.init(args=args)
